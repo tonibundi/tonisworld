df65c9a9f7aff318e660a3a17a9dad5db70d7734
tonibundi <antonia.bundi@icloud.com>
2024-08-25 14:12:00 +0200

    GH Pages setup

--- astro.config.mjs
+++ astro.config.mjs
@@ -31,8 +31,8 @@ const oklchToHex = (str) => {
 
 // https://astro.build/config
 export default defineConfig({
-  site: "https://fuwari.vercel.app/",
-  base: "/",
+  site: "https://tonibundi.github.io/",
+  base: "/tonisworld",
   trailingSlash: "always",
   integrations: [
     tailwind(),
